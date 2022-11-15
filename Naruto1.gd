4b8773b0837811cd36f3d58e5f16cf097ab83497
Oussema Lahami <oussemalahami1995@gmail.com>
2022-11-15 19:19:35 +0100

    first animation for kid naruto run

---
+++ Naruto1.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+export var speed = 600
+var screen_size = Vector2.ZERO
+
+
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+ 
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y < 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_h = false
+	elif velocity.y >0:
+		$AnimatedSprite.animation = "down"
+		$AnimatedSprite.flip_h = false
