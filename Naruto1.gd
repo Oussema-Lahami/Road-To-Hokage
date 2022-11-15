caa63f4df0b6df9964a15fdc98cbdcae07674cac
Oussema Lahami <oussemalahami1995@gmail.com>
2022-11-15 22:44:53 +0100

    updated smooth movement main character

--- Naruto1.gd
+++ Naruto1.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
-export var speed = 200
-var direction = Vector2(1,0)
+const MAX_SPEED = 300
+const ACCELERATION = 50
+const FRICTION = 50
 var velocity = Vector2.ZERO
-var screen_size = Vector2.ZERO
+
+var direction = Vector2(1,0)
+
 const SHURIKEN = preload("res://shuriken.tscn")
 
 func _process(delta):
@@ -11,34 +14,43 @@ func _process(delta):
 		$AnimatedSprite.play("throw")
 
 func _physics_process(delta):
+	#player movement start
+	var input_vector = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		$AnimatedSprite.play("right")
 		$AnimatedSprite.flip_h = false
-		velocity.x = speed
 		if sign($Position2D.position.x) == -1:
 			$Position2D.position.x *= -1
 	elif Input.is_action_pressed("ui_left"):
 		$AnimatedSprite.play("right")
 		$AnimatedSprite.flip_h = true
-		velocity.x = -speed
 		if sign($Position2D.position.x) == 1:
 			$Position2D.position.x *= -1
 	elif Input.is_action_pressed("ui_up"):
 		$AnimatedSprite.play("up")
-		velocity.y = -speed
 		if sign($Position2D.position.y) == -1:
 			$Position2D.position.y *= -1
 	elif Input.is_action_pressed("ui_down"):
 		$AnimatedSprite.play("down")
-		velocity.y = speed
 		if sign($Position2D.position.x) == 1:
 			$Position2D.position.x *= -1
 	else :
 		$AnimatedSprite.play("stanceRight")
-		velocity.x = 0
-		velocity.y = 0
-		
-		
+
+
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO , FRICTION * delta)
+	
+	move_and_collide(velocity)
+
+	#throw shuriken option by pressing "c" 
 	if Input.is_action_just_pressed("throw"):
 		$AnimatedSprite.play("throw")
 		var shuriken = SHURIKEN.instance()
@@ -46,4 +58,3 @@ func _physics_process(delta):
 			shuriken.set_shuriken_direction(-1)
 		get_parent().add_child(shuriken)
 		shuriken.global_position = $Position2D.global_position
-	velocity = move_and_slide(velocity)
--- bullet.gd
+++ bullet.gd
@@ -5,6 +5,7 @@ var direction = 1
 
 func set_shuriken_direction(dir):
 	direction = dir
+
 	
 func _physics_process(delta):
 	$AnimatedSprite.play("default")
