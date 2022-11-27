cf88b72ee87ab72eeb2353b55426cce189a8b823
Oussema Lahami <oussemalahami1995@gmail.com>
2022-11-27 14:17:42 +0100

    added animation attack for hashirama

---
+++ hashirama.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+var DRAGON = preload("res://dragon.tscn")
+
+
+
+#just to test later to be added ai
+func _physics_process(delta):
+	if Input.is_action_just_pressed("throw"):
+		$Timer.start()
+		$AnimatedSprite.play("default")
+
+	
+
+
+func _on_Timer_timeout():
+		var dragon = DRAGON.instance()
+		if $AnimatedSprite.flip_h == true: 
+			dragon.flip()
+		get_parent().add_child(dragon)
+		dragon.global_position = $Position2D.global_position
