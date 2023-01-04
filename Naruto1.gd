948d1a5fd2965035eb215673dd4bfbc4ca6acea1
Oussema Lahami <oussemalahami1995@gmail.com>
2023-01-04 19:38:35 +0100

    added stamina for when you transform

--- Naruto1.gd
+++ Naruto1.gd
@@ -83,6 +83,7 @@ func _physics_process(delta):
 		get_parent().add_child(shuriken)
 		shuriken.global_position = $Position2D.global_position
 	# option to transform into second form animation by pressing "X"
+	
 	if Input.is_action_just_pressed("transform") && !secondForm:
 		add_child(smoke)
 		smoke.get_node("Position2D").position = $Position2D2.position
@@ -91,13 +92,16 @@ func _physics_process(delta):
 		new.get_node("Camera2D").starting()
 		secondForm = true
 		$AnimatedSprite.visible = false
+		$healthBar.visible = false
 		if $AnimatedSprite.flip_h == true: 
 			new.set_form_direction()
 		get_parent().add_child(new)
 		new.global_position = $Position2D2.global_position
 		$Timer.start()
+
+
 	# option to go back to first form after transforming by pressing "Q"
-	if Input.is_action_just_pressed("goBack") && secondForm :
+	if (Input.is_action_just_pressed("goBack") && secondForm ):
 		add_child(smoke)
 		smoke.get_node("Position2D").position = $Position2D2.position
 		smoke.set_z_index(1000)
@@ -108,7 +112,22 @@ func _physics_process(delta):
 		$CollisionShape2D.disabled = false
 		$Camera2D.current = true
 		$Position2D2.global_position = naruto3.global_position
-		naruto3.queue_free()
+		naruto3.visible = false
+	
+	# going back when running out of chakra
+	if naruto3.get_node("chakraBar/ChakraOver").value == 0 && secondForm:
+		add_child(smoke)
+		smoke.get_node("Position2D").position = $Position2D2.position
+		smoke.set_z_index(1000)
+		smoke.play()
+		get_node("Camera2D").starting()
+		secondForm = false
+		$AnimatedSprite.visible = true
+		$CollisionShape2D.disabled = false
+		$Camera2D.current = true
+		$Position2D2.global_position = naruto3.global_position
+		naruto3.visible = false
+
 
 func _on_Timer_timeout():
 	new.delete()
---
+++ chakraBar.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+onready var health_over = $ChakraOver
+
+func _on_health_updated(health , amount):
+	health_over.value = health
+
+
+func _on_max_health_updated(max_health):
+	health_over.max_value = max_health
+
+
+func _on_Timer_timeout():
+	_on_health_updated(health_over.value - 10 , 5)
