68df192e6242b527ed949c51d15ab7057f2e7b0c
Oussema Lahami <oussemalahami1995@gmail.com>
2023-01-13 16:00:30 +0100

    added lord itachi main enemy in level 2 version 2.0

--- itachi v2.gd
+++ itachi v2.gd
@@ -1,9 +1,31 @@
 extends KinematicBody2D
+var KATON = preload("res://katon.tscn")
 
 
 func _ready():
 	$AnimatedSprite.play("stance")
 
 
-func _on_Area2D_area_entered(area):
+
+func _on_shoot_timeout():
+	var katon = KATON.instance()
+	if $AnimatedSprite.flip_h == true: 
+		katon.set_katon_direction(Vector2(-1 , -1))
+	get_parent().add_child(katon)
+	katon.global_position = $Position2D.global_position
+	$AnimatedSprite.play("stance")
+
+
+func _on_attack2_area_entered(area):
+	$katon.play()
+	$katonStop.start()
 	$AnimatedSprite.play("katon")
+	$shoot.start()
+
+
+func _on_attack2_area_exited(area):
+	$AnimatedSprite.play("stance")
+
+
+func _on_katonStop_timeout():
+	$katon.stop()
