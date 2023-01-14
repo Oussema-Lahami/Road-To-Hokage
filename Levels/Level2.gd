09978187b7062327403c2e22f6b2d0a8cf1f9478
Aziz-fer <64703046+Aziz-fer@users.noreply.github.com>
2023-01-14 06:13:26 +0100

    allah la trabhou li 3almek langlais

--- Levels/Level2.gd
+++ Levels/Level2.gd
@@ -3,25 +3,17 @@ var levelname = preload("res://Levels/LevelName.tscn")
 const SMOKE = preload("res://smoke.tscn")
 var smoke = SMOKE.instance()
 
-
 func _ready():
 	$narutoyezzy.start()
-	
 	Global.current_level=2
 	$NarutoTalk.play()
 	yield(get_tree().create_timer(3), "timeout")
 	var level_name = levelname.instance()
 	add_child(level_name)
-	#smoke.scale.x = 2
-	#smoke.scale.y = 2
 	$Timer.start()
-
 
 func _on_Timer_timeout():
 	$song.play()
-	#$starting.stop()
-	#Global.play_music()
-	pass
 
 func _on_timerdead_timeout():
 	$ravens.visible = true
